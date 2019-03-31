a6cece66d3756869d6506068e14bb122245d26ed
ahmetbedir <a_bedir43@hotmail.com>
2019-03-31 07:24:10 +0300

    Kontrol yapısı değiştir, yeni methodlar eklendi

---
+++ form.php
@@ -0,0 +1,17 @@
+<form action="" method="POST">
+    <input type="hidden" name="_method" value="PUT">
+    <fieldset>
+        <legend>Login</legend>
+        <section>
+            <label>Kullanıcı Adı:</label>
+            <input type="text" name="name">
+        </section>
+        <hr>
+        <section>
+            <label>Şifre:</label>
+            <input type="password" name="surname">
+        </section>
+        <hr>
+        <button type="submit">Gönder</button>
+    </fieldset>
+</form>
--- routes.php
+++ routes.php
@@ -2,19 +2,19 @@
 
 use BDR\Route;
 
-/*Route::get('/home', function ($args) {
-//
-});*/
+Route::get('/', function(){
+	return include('form.php');
+});
 
-// Route::get('/user/{id}/edit/{name}', function ($id = null, $name = null) {
-// 	echo "Düzenleme sayfası: ID = {$id}, name = $name";
-// });
+Route::match(['get', 'post'] ,'/', function(){
+	echo "<pre>";
+	print_r($_REQUEST);
+});
 
-// Route::get('/user/profile/{id}', function ($id) {
-// 	echo "Üye profil sayfası: ID =" . $id;
-// });
+Route::get('/user/{id}/edit', function ($id) {
+	echo "Düzenleme sayfası: ID: $id";
+});
 
-
-Route::get('/user/profile/{id}/{deneme}', function ($id) {
-	echo "Üye profil sayfası2: ID =" . $id;
+Route::get('/user/profile/{id}', function ($id) {
+ 	echo "Üye profil sayfası: ID =" . $id;
 });
